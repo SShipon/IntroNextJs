310e34055ed2114b6a08de7e62741715bddca3b1
SShipon <nxshipon@gmail.com>
2024-09-07 06:39:55 -0700

    next nested routing add

---
+++ app/news/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsHomPage = () => {
+    return (
+        <div>
+            <h2 className='text-center text-4xl text-rose-500'>News home page add</h2>
+        </div>
+    );
+};
+
+export default NewsHomPage;
